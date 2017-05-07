79f36ba36785335c6a09581d7379b6eb161f168b
digibson <dave0710@googlemail.com>
2017-05-07 20:31:04 +0100

    Update button_interrupt.py

--- button_interrupt.py
+++ button_interrupt.py
@@ -1,28 +1,16 @@
-#!/usr/bin/env python2.7  
-# script by Alex Eames http://RasPi.tv/  
-# http://raspi.tv/2013/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio  
+#!/usr/bin/env python3.4
+#should check the above line for latest version
+# need to run sudo apt-get install python-rpi.gpio python3-rpi.gpio
 import RPi.GPIO as GPIO  
 GPIO.setmode(GPIO.BCM)  
   
-# GPIO 23 set up as input. It is pulled up to stop false signals  
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+#Initialise pin 21 with pullup  
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
   
-print "Make sure you have a button connected so that when pressed"  
-print "it will connect GPIO port 23 (pin 16) to GND (pin 6)\n"  
-raw_input("Press Enter when ready\n>")  
-  
-print "Waiting for falling edge on port 23"  
-# now the program will do nothing until the signal on port 23   
-# starts to fall towards zero. This is why we used the pullup  
-# to keep the signal high and prevent a false interrupt  
-  
-print "During this waiting time, your computer is not"   
-print "wasting resources by polling for a button press.\n"  
-print "Press your button when ready to initiate a falling edge interrupt."  
+# Wait for falling edge 
 try:  
-    GPIO.wait_for_edge(23, GPIO.FALLING)  
-    print "\nFalling edge detected. Now your program can continue with"  
-    print "whatever was waiting for a button press."  
+    GPIO.wait_for_edge(21, GPIO.FALLING)  
+    print("Button pressed /n")  
 except KeyboardInterrupt:  
     GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
 GPIO.cleanup()           # clean up GPIO on normal exit
