f6316c8887c49a80ffa9fe113336be3c34eb11d2
digibson <dave0710@googlemail.com>
2017-05-07 20:47:36 +0100

    Update button_interrupt_callback.py

--- button_interrupt_callback.py
+++ button_interrupt_callback.py
@@ -3,10 +3,7 @@
   
 import RPi.GPIO as GPIO  
 GPIO.setmode(GPIO.BCM)  
-  
-# GPIO 23 & 24 set up as inputs. One pulled up, the other down.  
-# 23 will go to GND when button pressed and 24 will go to 3V3 (3.3V)  
-# this enables us to demonstrate both rising and falling edge detection  
+
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
 GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
   
@@ -20,11 +17,11 @@ def take_picture(channel):
 # else is happening in the program, the function "my_callback" will be run  
 # It will happen even while the program is waiting for  
 # a falling edge on the other button.  
-GPIO.add_event_detect(24, GPIO.FALLING, callback=take_picture)  
+GPIO.add_event_detect(20, GPIO.FALLING, callback=take_picture)  
   
 try:  
     print "Waiting for falling edge on port 23"  
-    GPIO.wait_for_edge(23, GPIO.FALLING)  
+    GPIO.wait_for_edge(21, GPIO.FALLING)  
     print("Shutdown function.")  
   
 except KeyboardInterrupt:  
